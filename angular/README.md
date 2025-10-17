3e75bd871526936c63226c8134172d177130f308
Zhelizko Daniel <yokzz.programming@gmail.com>
2025-10-17 14:51:59 +0300

    new angular project

--- angular/README.md
+++ angular/README.md
@@ -1,4 +1,4 @@
-# FrontendLearner
+# Angular
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.3.6.
 
--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Register } from './auth/register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Register],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend_learner');
+  protected readonly title = signal('angular');
 }
